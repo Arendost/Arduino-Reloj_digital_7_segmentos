1aaf481b081712cdd2382d1bcd0712d1ed8d38e6
Abraham Rodriguez <38869805+Arendost@users.noreply.github.com>
2021-02-04 13:32:24 -0400

    Create off.ino

---
+++ off.ino
@@ -0,0 +1,11 @@
+void off()
+{
+  digitalWrite(10,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(9,LOW);
+
+}
